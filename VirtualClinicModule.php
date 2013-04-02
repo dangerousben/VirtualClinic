ff94881ac54eec87125328da16cef377ed89ff2a
zendawg <rich@statefive.org>
2013-04-02 17:07:27 +0100

    Wild goose chase trying to sort out assets for the VC (EyeGraph JS import was required); working now.

--- VirtualClinicModule.php
+++ VirtualClinicModule.php
@@ -1,31 +1,17 @@
 <?php
 
-class VirtualClinicModule extends BaseEventTypeModule
-{
-	public function init()
-	{
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
+class VirtualClinicModule extends BaseEventTypeModule {
 
-		// import the module-level models and components
-		$this->setImport(array(
-			'VirtualClinic.models.*',
-			'VirtualClinic.views.*',
-			'VirtualClinic.components.*',
-			'VirtualClinic.controllers.*',
-		));
-		parent::init();
-	}
+  public function init() {
+    // this method is called when the module is being created
+    // you may place code here to customize the module or the application
+    // import the module-level models and components
+    $this->setImport(array(
+        'VirtualClinic.models.*',
+        'VirtualClinic.components.*',
+    ));
 
-	public function beforeControllerAction($controller, $action)
-	{
-		if(parent::beforeControllerAction($controller, $action))
-		{
-			// this method is called before any module controller action is performed
-			// you may place customized code here
-			return true;
-		}
-		else
-			return false;
-	}
+    parent::init();
+  }
+
 }
--- models/SummaryView.php
+++ models/SummaryView.php
@@ -3,7 +3,6 @@
 /**
  * OpenEyes
  *
- * (C) University of Cardiff, 2012
  * (C) OpenEyes Foundation, 2011-2012
  * This file is part of OpenEyes.
  * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
@@ -13,7 +12,6 @@
  * @package OpenEyes
  * @link http://www.openeyes.org.uk
  * @author OpenEyes <info@openeyes.org.uk>
- * @copyright Copyright (c) University of Cardiff, 2012
  * @copyright Copyright (c) 2011-2012, OpenEyes Foundation
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
