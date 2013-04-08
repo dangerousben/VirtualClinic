7636caa3383feb2e6b3a3014e0e8122a44b50301
zendawg <rich@statefive.org>
2013-04-08 10:59:27 +0100

    Moved code across from the glaucoma clinic to the VC; this is now functional code for all other clinics.

--- models/VirtualClinic.php
+++ models/VirtualClinic.php
@@ -119,7 +119,7 @@ class VirtualClinic {
             Yii::import($module_name, true);
             if (class_exists($class_name, false)) {
                 $clinic = new $class_name();
-                $value = $clinic->getColumnValue($pid, $subspeciality, $columnName);
+                $value = $this->getColumnValues($pid, $subspeciality, $columnName, $clinic->columns[$columnName]);
             }
         } catch (Exception $ex) {
             // TODO how to deal with exception? Report or just leave?
@@ -149,6 +149,115 @@ class VirtualClinic {
             $text = $data;
         }
         return $text;
+    }
+
+    /**
+     * 
+     * @param type $pid
+     * @param type $speciality
+     * @param type $columnName
+     * @return type
+     */
+    public function getColumnValues($pid, $speciality, $columnName, $col) {
+        $event_type = $col['event_type'];
+        $class_name = $col['class_name'];
+        $field = $col['field'];
+        $f = new $class_name();
+        $obj = $this->getElementForLatestEventInEpisode($pid, $event_type, $f, $speciality, $columnName);
+        $ret = null;
+        if ($obj) {
+            if (is_array($field)) {
+                $ret = array();
+                foreach ($field as $fld) {
+                    array_push($ret, $this->getLeafMember($obj, $fld));
+                }
+            } else {
+                $ret = $obj->$field;
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * 
+     * @param type $obj
+     * @param type $children
+     */
+    function getLeafMember($obj, $children) {
+        $ret = null;
+        if (count($children) > 1) {
+            $ret = $this->getLeafMember($obj[$children[0]], array_slice($children, 1));
+        } else {
+            $ret = $obj[$children[0]];
+        }
+        return $ret;
+    }
+
+    /**
+     * Gets the specified event type, if it exists.
+     * 
+     * @param string $field the class name of the event type to get.
+     * 
+     * @return Event if it exists; null otherwise.
+     */
+    private function getEventType($field) {
+        $criteria = new CdbCriteria;
+        $criteria->compare('class_name', $field);
+        $criteria->distinct = true;
+        return EventType::model()->find($criteria);
+    }
+
+    /**
+     * Gets the episode associated with the patient for the given speciality.
+     * 
+     * Adapted from the Patient method of the same name. Since patient contains
+     * more data than this class, it has been re-implemented here (more
+     * specifically, in a clinic no single patient is considered).
+     * 
+     * @param type $patient_id the patient to get the episode for.
+     * 
+     * @return type
+     */
+    public function getEpisodeForCurrentSubspecialty($patient_id) {
+        $firm = Firm::model()->findByPk(Yii::app()->session['selected_firm_id']);
+
+        $ssa = $firm->serviceSubspecialtyAssignment;
+
+        // Get all firms for the subspecialty
+        $firm_ids = array();
+        foreach (Firm::model()->findAll('service_subspecialty_assignment_id=?', array($ssa->id)) as $firm) {
+            $firm_ids[] = $firm->id;
+        }
+
+        return Episode::model()->find('patient_id=? and firm_id in (' . implode(',', $firm_ids) . ')', array($patient_id));
+    }
+
+    /**
+     * 
+     * @param int $patient_id
+     * @param type $event_class
+     * @param type $element
+     * @param type $speciality
+     * @param type $columnName
+     * @return type
+     */
+    public function getElementForLatestEventInEpisode($patient_id, $event_class, $element, $speciality, $columnName) {
+        $event_type = $this->getEventType($event_class);
+
+        if ($episode = $this->getEpisodeForCurrentSubspecialty($patient_id)) {
+            if ($event = $episode->getMostRecentEventByType($event_type->id)) {
+                $criteria = new CDbCriteria;
+                $epid = $episode->id;
+                $eid = $event->id;
+                $criteria->compare('episode_id', $episode->id);
+                $criteria->compare('event_id', $event->id);
+                $criteria->order = 'datetime desc';
+
+                return $element::model()
+                                ->with('event')
+                                ->find($criteria);
+            }
+        }
     }
 
 }
